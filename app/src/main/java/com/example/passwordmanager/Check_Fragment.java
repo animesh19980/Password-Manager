49107be22a891a1e733c5b84987a5cf0086a1c8c
Animesh <animesh0221@gmail.com>
2023-08-26 17:38:16 +0530

    1

--- app/src/main/java/com/example/passwordmanager/Check_Fragment.java
+++ app/src/main/java/com/example/passwordmanager/Check_Fragment.java
@@ -18,6 +18,9 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Check_Fragment extends Fragment {
     private EditText etPassword;
@@ -53,11 +56,19 @@ public class Check_Fragment extends Fragment {
 
                 if (password.length() == 0) {
                     tvPasswordError.setVisibility(View.VISIBLE);
-                } if (password.equals("098#@Black")) {
-                    PasswordError.setVisibility(View.VISIBLE);
-                } else {
-                    checkdone.setVisibility(View.VISIBLE);
+                    return;
                 }
+
+                List<String> forbiddenPasswords = Arrays.asList("Password@012","Password@123","Password@234","Password@345","Password@456","Password@567","Password@678","Password@789","Password@890","Love!234","Summer21","Hello!45","Pizza!123","Coffee#1","Friend$7","Choco#8t","Beach!7s","Winter#68","Sport$45","Dream!21","T3aT!m34","Flower$6","Star!789","Golden#4","Mount#45","Fishes$2","Light#67","Play!m8","Read!n#2","Corn#789","Rain$567","Moon!345","Song$123","Wind#678","Bard$234","Joker#56","Film$234","Drink#78","Plant$12","Cat$!789","Ball#123","Party$67","Tree$!89","Cake$456","Milk#!23","Dog$#567","Lamp!890","Book#234","Farm$678","Toy$!123","Soap$!67","Gift$#89","Fox$!234","Hot#5678","Pool!123","Bird$!90");
+
+                for (String forbiddenPassword : forbiddenPasswords) {
+                    if (password.equals(forbiddenPassword)) {
+                        PasswordError.setVisibility(View.VISIBLE);
+                        return;
+                    }
+                }
+
+                checkdone.setVisibility(View.VISIBLE);
             }
         });
         inputChange();
